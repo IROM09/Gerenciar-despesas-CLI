c62a764d60be2133f7e619bae40941223f380c18
Izac Oliveira <izacrios.moraes@gmail.com>
2026-04-03 15:59:27 -0300

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,52 @@
+from main import categoria
+ob = categoria()
+print("""
+////////////////|////////////////
+////////  Bem vindo ao  /////////
+////  Gerenciador de gastos  ////
+///                           ///""")
+while True:
+    act = int(input("""///                           ///
+//// O que você quer fazer?  ////
+/// 1 - Adicionar gasto       ///
+/// 2 - Ver gastos            ///
+/// 3 - Extrair               ///
+/// 4 - Sair                  ///
+///                           ///
+   R: """))
+
+    if act == 1:
+        nome = str(input("""///                           ///
+//// Digite o nome do produto ///
+///                           ///
+   R:"""))
+        cat = int(input("""///                           ///
+/////// Qual a categoria? ///////
+/// 1 - Comida                ///
+/// 2 - Higiene               ///
+/// 3 - Casa                  ///
+/// 4 - Lazer                 ///
+/// 5 - Imprevisto            ///
+///                           ///
+   R:"""))
+        des = str(input("""///                           ///
+/////// Digite a descrição //////
+////// ou motivo da compra //////
+///                           ///
+   R:"""))
+        val = float(input("""///                           ///
+///////// Digite o valor ////////
+///                           ///
+   R:"""))
+        print("""///                           ///""")
+        ob.atribuir(nome, cat, des, val)
+    elif act == 2:
+        ob.ler()
+    elif act == 3:
+        ob.extrair()
+    elif act == 4:
+        print("""///                           ///
+////// Obrigado por usar o //////
+////  Gerenciador de gastos  ////
+////////////////|////////////////""")
+        break
---
+++ main.py
@@ -0,0 +1,78 @@
+class categoria:
+    def __init__(self):
+        self.comida = {}
+        self.higiene = {}
+        self.casa = {}
+        self.lazer = {}
+        self.imprevisto = {}
+    def atribuir(self, nome, cat, des, val):
+        if  cat == 1:
+            self.comida.update({nome:(des, val)})
+        elif cat == 2:
+            self.higiene.update({nome:(des, val)})
+        elif cat == 3:
+            self.casa.update({nome:(des, val)})
+        elif cat == 4:
+            self.lazer.update({nome:(des, val)})
+        elif cat == 5:
+            self.imprevisto.update({nome:(des, val)})
+    def ler(self):
+        if self.comida != {}:
+            print("""///                           ///
+///                           ///
+////// Gastos de Comida ////////
+///                           ///""")
+            for nome, (des, val) in self.comida.items():
+                print(f"""  {nome}: {des} - R$ {val:.2f}
+///                           ///""")
+        if self.higiene != {}:
+            print("""///                           ///
+///                           ///
+////// Gastos de Higiene ////////
+///                           ///""")
+            for nome, (des, val) in self.higiene.items():
+                print(f"""  {nome}: {des} - R$ {val:.2f}
+///                           ///""")
+        if self.casa != {}:
+            print("""///                           ///
+///                           ///
+//////// Gastos de Casa /////////
+///                           ///""")
+            for nome, (des, val) in self.casa.items():
+                print(f"""  {nome}: {des} - R$ {val:.2f}
+///                           ///""")
+        if self.lazer != {}:
+            print("""///                           ///
+///                           ///
+//////// Gastos de Lazer ////////
+///                           ///""")
+            for nome, (des, val) in self.lazer.items():
+                print(f"""  {nome}: {des} - R$ {val:.2f}
+///                           ///""")
+        if self.imprevisto != {}:
+            print("""///                           ///
+///                           ///
+///// Gastos de Imprevisto //////
+///                           ///""")
+            for nome, (des, val) in self.imprevisto.items():
+                print(f"""  {nome}: {des} - R$ {val:.2f}
+///                           ///""")
+    def extrair(self):
+        with open("gastos.json", "w", encoding="UTF-8") as arq:
+            cont = 0
+            for nome, (des, val) in self.comida.items():
+                arq.write(f"""  "{nome}": {{"descrição": "{des}", "valor": {val}}},\n""")
+                cont += 1
+            for nome, (des, val) in self.higiene.items():
+                arq.write(f"""  "{nome}": {{"descrição": "{des}", "valor": {val}}},\n""")
+                cont += 1
+            for nome, (des, val) in self.casa.items():
+                arq.write(f"""  "{nome}": {{"descrição": "{des}", "valor": {val}}},\n""")
+                cont += 1
+            for nome, (des, val) in self.lazer.items():
+                arq.write(f"""  "{nome}": {{"descrição": "{des}", "valor": {val}}},\n""")
+                cont += 1
+            for nome, (des, val) in self.imprevisto.items():
+                arq.write(f"""  "{nome}": {{"descrição": "{des}", "valor": {val}}},\n""")
+                cont += 1
+        print(f"""/// Arquivo salvo com sucesso ///""")
